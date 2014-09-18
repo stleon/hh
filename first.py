ab9e89c25bdfa2326591ad82e34e69b8a4a535a8
ST LEON <leonst998@gmail.com>
2014-09-18 13:27:56 +0400

    generator
    generator in first

--- first.py
+++ first.py
@@ -1,3 +1,3 @@
 from statistics import median
 
-print(median(int(i) for i in '{}{}'.format(input('Enter first sorted numeric array (1 2 3 4): '), input('Enter second sorted numeric array (1 4 5 6): ')) if i != ' '))
+print(median((int(i) for i in '{}{}'.format(input('Enter first sorted numeric array (1 2 3 4): '), input('Enter second sorted numeric array (1 4 5 6): ')) if i != ' ')))
