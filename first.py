2484e654c1307022dc67e8f8b76f98bfe9fd0f95
ST LEON <leonst998@gmail.com>
2014-09-25 23:57:35 +0400

    !set
    without set

--- first.py
+++ first.py
@@ -2,4 +2,4 @@ from statistics import median
 
 st = '{} {}'.format(input('Enter first sorted numeric array (1 2 3 4): '), input('Enter second sorted numeric array (1 4 5 6): '))
 
-print(median(set(map(int, st.split()))))
+print(median(map(int, st.split())))
